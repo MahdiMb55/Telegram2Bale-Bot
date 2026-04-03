d32f166bb35c2f213af627579fd5f239fdc5ae8c
Mahdi Akbari <mahdim2f4@gmail.com>
2026-04-04 00:00:38 +0330

    add proxy

--- telegram_client.py
+++ telegram_client.py
@@ -1,6 +1,12 @@
 from telethon import TelegramClient, events
 import os
 
+proxy = (
+    socks.SOCKS5,
+    "127.0.0.1",   # آدرس proxy
+    1080           # پورت proxy
+)
+
 class TelegramListener:
     def __init__(self, config, dispatcher):
         self.config = config
@@ -9,7 +15,8 @@ class TelegramListener:
         self.client = TelegramClient(
             config["telegram"]["session"],
             config["telegram"]["api_id"],
-            config["telegram"]["api_hash"]
+            config["telegram"]["api_hash"],
+            proxy=proxy
         )
 
     async def start(self):
